122e649c331c83ba3e3355fcd281d1335cbc254c
Junho Kim <78329855+JunnyCoder@users.noreply.github.com>
2023-03-29 18:59:50 +0000

    practiced

--- practice/decorator.py
+++ practice/decorator.py
@@ -3,20 +3,65 @@
 '''
 from functools import wraps
 # import datetime
-# import time
+import time
 
-def test_deco(func):
-    '''Decorator testing'''
+def logger ():
+    '''로그 펑션'''
+    print(time.time())
+
+def arg_check_type(func):
+    '''
+    주어진 파라미터의 타입을 체크
+    딕셔너리 타입이용 시 list_, str_ 등의 정확한 형식 요구사항을 가장 앞에 작성해야함 
+    '''
+    @wraps(func)
+    def pct_inner(arg_type, *args, **kwargs):
+        if arg_type is not None :
+            for arg in range(len(args)):
+                if not isinstance(arg, arg_type):
+                    return f"ERR_{args.index(arg)} is not type{arg_type}"
+            return func(*args)
+        elif arg_type is None :
+            for key, value in kwargs:
+                if not isinstance(value, key.split("_")[0]):
+                    return f"ERR_{value} is not type{key}"
+            return func(*kwargs)
+    return pct_inner
+
+def parm_check_match(func):
+    '''주어진 파라미터의 일치여부를 체크'''
+    @wraps(func)
+    def pcm_inner(*args, **kwargs):
+        return func(*args, **kwargs)
+    return pcm_inner
+
+def deco_log(func):
+    '''로그 데코레이터'''
     @wraps(func)
     def innerfunc(*args, **kwargs):
+        logger()
         return func(*args, **kwargs)
     return innerfunc
 
-@test_deco
-def roller ():
-    pass
+# @deco_log
+# def accesser (a):
+#     '''접근하는 펑션'''
+#     print('Accessed', a)
+#     return a
 
+# a = accesser('a')
+# b = accesser
+# print(b)
 
+# b('')
+# print(b)
+
+# b('w')
+# print(b)
+
+# b(a)
+
+# print(a)
 
 
 
